5667c0b4c560ec1835b32d18dae83deeb436bf7b
enrique <enriqueonaga@gmail.com>
2023-08-28 23:36:00 -0300

    change button's message

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ function changeBlack(event){
     square.style.backgroundColor = "black";
 }
 
-const container = document.querySelector("#container");
-const side = 16;
+function generate() {
+    let input = prompt("Please enter a side value:");
+    return input;
+}
 
-for(let i = 1; i <= side * side; i++){
+const container = document.querySelector("#container");
+input = 16;
+
+for(let i = 1; i <= input * input; i++){
     const square = document.createElement("div");
     square.classList.add("square");
+
+    const percent = 100/(input);
+    square.setAttribute("style", `height: ${percent}%; width: ${percent}%; outline:solid black;`);   
 
     square.addEventListener("mouseover", changeBlack);
     container.appendChild(square);
 }
+
+const btn = document.querySelector("#btn");
+btn.addEventListener('click', () => {
+    let input = prompt("Please enter a side value:");
+});
+
+console.log(input);
+
+
 
 
 
