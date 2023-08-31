0ee30441f7091fdd67a96999ad7a55f4b677bb0c
enrique <enriqueonaga@gmail.com>
2023-08-30 23:33:50 -0300

    update js logic

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 function changeBlack(event){
-    const square = event.target;
+    let square = event.target;
     square.style.backgroundColor = "black";
 }
 
-function generate() {
-    //algum codigo que apague o container anterior... 
-    let input = prompt("Please enter a side value:");
+function generate(){
+    container.innerHTML = "";
+
+    let input = prompt("Please enter a side value (limit: 100):");
+
+    while (input > 100){
+        let input = prompt("Please enter another side value (limit: 100):");
+    }
 
     for(let i = 1; i <= input * input; i++){
-        const square = document.createElement("div");
+        let square = document.createElement("div");
         square.classList.add("square");
     
-        const percent = 100/(input);
-        square.setAttribute("style", `height: ${percent}%; width: ${percent}%; outline:solid black;`);   
+        let percent = 100/(input);
+        square.setAttribute("style", `height: ${percent}%; width: ${percent}%; outline:solid;`);   
     
         square.addEventListener("mouseover", changeBlack);
         container.appendChild(square);
     }
 }
 
-for(let i = 1; i <= 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-
-    const percent = 100/(16);
-    square.setAttribute("style", `height: ${percent}%; width: ${percent}%; outline:solid black;`);   
-
-    square.addEventListener("mouseover", changeBlack);
-    container.appendChild(square);
-}
-
-
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", generate);
+
 
 
 
